bd9585f58f635269e871d73cbe070ab5d355bbc7
Rishi G. <mail.rishig@gmail.com>
2025-11-01 13:39:15 +0100

    minor changes

--- project1/src/pages/Homepage.jsx
+++ project1/src/pages/Homepage.jsx
@@ -1,16 +1,25 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+import foto from "../assets/overige_foto.png";
 
 const Homepage = () => {
   return (
     <>
       <Navbar />
       <section className="homePage">
-        <article className="home">Menu</article>
-        <article className="home">Locatie</article>
-        <article className="home">Contact</article>
-        <article className="home">About us</article>
+        <article className="home">
+          <h3>Menu</h3> <img src={foto} width="120px" />
+        </article>
+        <article className="home">
+          <h3>Locatie</h3> <img src={foto} width="120px" />
+        </article>
+        <article className="home">
+          <h3>Contact</h3> <img src={foto} width="120px" />
+        </article>
+        <article className="home">
+          <h3>About us </h3> <img src={foto} width="120px" />
+        </article>
       </section>
       <Footer />
     </>
