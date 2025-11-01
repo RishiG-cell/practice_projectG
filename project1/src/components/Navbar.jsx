3d3f1be957e37f29168196a42bbcb3d49f9f7e21
Rishi G. <mail.rishig@gmail.com>
2025-11-01 12:59:00 +0100

    first commit

---
+++ project1/src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import logo from "../assets/het_oosten.png";
+
+const Navbar = () => {
+  return (
+    <div className="nav">
+      <img src={logo} alt="logo" />
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ project1/src/pages/Homepage.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+
+const Homepage = () => {
+  return (
+    <>
+      <Navbar />
+      <section className="homePage">
+        <article className="home">Menu</article>
+        <article className="home">Locatie</article>
+        <article className="home">Contact</article>
+        <article className="home">About us</article>
+      </section>
+    </>
+  );
+};
+
+export default Homepage;
